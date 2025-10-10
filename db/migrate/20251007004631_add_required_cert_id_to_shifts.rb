6bcf236cf8729163341d83caec25fa3e5b5a1a56
kishanssg <kishansai55555@gmail.com>
2025-10-10 01:47:40 -0400

    FIX: Correct Rails migration version from 8.0 to 7.2
    The migration was using ActiveRecord::Migration[8.0] which doesn't exist in Rails 7.2.
    This was preventing the required_cert_id migration from being applied on Heroku.
    
    Fixed to use ActiveRecord::Migration[7.2] to match the Rails version.

--- db/migrate/20251007004631_add_required_cert_id_to_shifts.rb
+++ db/migrate/20251007004631_add_required_cert_id_to_shifts.rb
@@ -1,4 +1,4 @@
-class AddRequiredCertIdToShifts < ActiveRecord::Migration[8.0]
+class AddRequiredCertIdToShifts < ActiveRecord::Migration[7.2]
   def change
     add_column :shifts, :required_cert_id, :bigint
     add_foreign_key :shifts, :certifications, column: :required_cert_id, on_delete: :nullify
