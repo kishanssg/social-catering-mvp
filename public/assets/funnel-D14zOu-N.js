0a0f4b4c389c94ab3544c2ce26954218f4762caa
kishanssg <kishansai55555@gmail.com>
2025-10-12 13:48:43 -0400

    Add React build files to public/assets for static serving

---
+++ public/assets/funnel-D14zOu-N.js
@@ -0,0 +1,6 @@
+import{t as r,c}from"./format-BqWGv6kL.js";import{c as o}from"./createLucideIcon-3h5MTQgh.js";function u(e,a,n){const t=r(e,n?.in);return isNaN(a)?c(e,NaN):(a&&t.setDate(t.getDate()+a),t)}/**
+ * @license lucide-react v0.545.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M10 20a1 1 0 0 0 .553.895l2 1A1 1 0 0 0 14 21v-7a2 2 0 0 1 .517-1.341L21.74 4.67A1 1 0 0 0 21 3H3a1 1 0 0 0-.742 1.67l7.225 7.989A2 2 0 0 1 10 14z",key:"sc7q7i"}]],d=o("funnel",s);export{d as F,u as a};
